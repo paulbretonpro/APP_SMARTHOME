902db14574608bb5545266fd8b8d2b5945e8aec4
paulbretonpro <paulobreton29800@gmail.com>
2023-07-25 11:05:00 +0700

    feat(loader): add loader dashbord

--- pages/index.vue
+++ pages/index.vue
@@ -4,21 +4,27 @@
     Dashboard for viewing api endpoint status
   </div>
   <div class="flex flex-col sm:flex-row flex-wrap gap-4">
-    <DashboardCardStatusApi
-      api="Home sensor"
-      :status="sensorApiIsActive.status"
-      :last-record="sensorApiIsActive.lastUpdate"
-    ></DashboardCardStatusApi>
-    <DashboardCardStatusApi
-      api="Weather"
-      :status="weatherApiIsActive.status"
-      :last-record="weatherApiIsActive.lastUpdate"
-    ></DashboardCardStatusApi>
-    <DashboardCardStatusApi
-      api="Energy consumption"
-      :status="captorApiIsActive.status"
-      :last-record="captorApiIsActive.lastUpdate"
-    ></DashboardCardStatusApi>
+    <InnerLoader :loading="sensorLoading">
+      <DashboardCardStatusApi
+        api="Home sensor"
+        :status="sensorApiIsActive.status"
+        :last-record="sensorApiIsActive.lastUpdate"
+      ></DashboardCardStatusApi>
+    </InnerLoader>
+    <InnerLoader :loading="weatherLoading">
+      <DashboardCardStatusApi
+        api="Weather"
+        :status="weatherApiIsActive.status"
+        :last-record="weatherApiIsActive.lastUpdate"
+      ></DashboardCardStatusApi>
+    </InnerLoader>
+    <InnerLoader :loading="captorLoading">
+      <DashboardCardStatusApi
+        api="Energy consumption"
+        :status="captorApiIsActive.status"
+        :last-record="captorApiIsActive.lastUpdate"
+      ></DashboardCardStatusApi>
+    </InnerLoader>
   </div>
 </template>
 <script setup lang="ts">
@@ -30,11 +36,15 @@ const sensorStore = useSensorStore();
 const weatherStore = useWeatherStore();
 const captorStore = useCaptorStore();
 
-await sensorStore.fetch();
-await weatherStore.fetch();
-await captorStore.fetch();
+useAsyncData("sensor", async () => await sensorStore.fetch());
+useAsyncData("weather", async () => await await weatherStore.fetch());
+useAsyncData("captor", async () => await captorStore.fetch());
 
 const sensorApiIsActive = computed(() => sensorStore.isActive);
 const weatherApiIsActive = computed(() => weatherStore.isActive);
 const captorApiIsActive = computed(() => captorStore.isActive);
+
+const sensorLoading = computed(() => sensorStore.loading);
+const weatherLoading = computed(() => weatherStore.loading);
+const captorLoading = computed(() => captorStore.loading);
 </script>
