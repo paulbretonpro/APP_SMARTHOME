fe35b11831fff2134ab47557dc6fcbaffa3eaf73
paulbretonpro <paulobreton29800@gmail.com>
2023-07-25 10:57:40 +0700

    feat(table): fix responsive, delete margin, add message empty data

--- components/Table.vue
+++ components/Table.vue
@@ -1,37 +1,42 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <div class="flex flex-col">
-    <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-        <div
-          class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg"
-        >
-          <table class="min-w-full divide-y divide-gray-300">
-            <thead class="bg-gray-50">
-              <tr>
-                <th
-                  v-for="(col, index) in columns"
-                  :key="index"
-                  scope="col"
-                  class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900"
-                >
-                  {{ col.label }}
-                </th>
-              </tr>
-            </thead>
-            <tbody class="divide-y divide-gray-200 bg-white">
-              <tr v-for="(d, index) in data" :key="index">
-                <td
-                  v-for="(pos, idx) in Object.entries(d).length"
-                  :key="idx"
-                  class="whitespace-nowrap pl-4 pr-3 py-4 text-sm text-gray-500"
-                >
-                  {{ matchColumnData(d, idx) }}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+    <div class="inline-block min-w-full py-2 align-middle">
+      <div
+        class="overflow-auto shadow ring-1 ring-black ring-opacity-5 rounded-lg"
+      >
+        <table class="min-w-full divide-y divide-gray-300">
+          <thead class="bg-gray-50">
+            <tr>
+              <th
+                v-for="(col, index) in columns"
+                :key="index"
+                scope="col"
+                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900"
+              >
+                {{ col.label }}
+              </th>
+            </tr>
+          </thead>
+          <tbody class="divide-y divide-gray-200 bg-white">
+            <tr v-for="(d, index) in data" :key="index">
+              <td
+                v-for="(pos, idx) in Object.entries(d).length"
+                :key="idx"
+                class="whitespace-nowrap pl-4 pr-3 py-4 text-sm text-gray-500"
+              >
+                {{ matchColumnData(d, idx) }}
+              </td>
+            </tr>
+          </tbody>
+          <tfoot v-if="data?.length === 0">
+            <tr class="font-bold text-sm text-slate-600">
+              <td colspan="100" class="text-center py-4">
+                No data available ... Please refresh
+              </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     </div>
   </div>
