414ae4818a9e8d35e037c51279bc686fe1863af1
paulbretonpro <paulobreton29800@gmail.com>
2023-07-27 09:47:54 +0700

    fix date

--- composables/useDate.ts
+++ composables/useDate.ts
@@ -8,14 +8,13 @@ export const useDate = () => {
     date.getFullYear() + "/" + getMonth(date) + "/" + getDay(date);
 
   const getMonth = (date: Date) =>
-    date.getMonth() >= 10 ? date.getMonth() : "0" + date.getMonth();
+    date.getMonth() >= 10 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1);
 
   const getDay = (date: Date) =>
-    date.getDay() >= 10 ? date.getDay() : "0" + date.getDay();
+    date.getDate() >= 10 ? date.getDate() : "0" + date.getDate();
 
   const fullDate = (date: Date | string) => {
     const dateTz = new Date(date);
-
     return getDate(dateTz) + " " + getHours(dateTz);
   };
 
