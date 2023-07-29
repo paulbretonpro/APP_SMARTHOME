5ba15850d052e573c084b0c0ba4fbc728e8d4734
paulbretonpro <paulobreton29800@gmail.com>
2023-07-30 01:03:12 +0700

    feat(btn): add btn white and disabled

--- components/Button.vue
+++ components/Button.vue
@@ -1,7 +1,14 @@
 <template>
   <button
     type="button"
-    class="h-fit w-full sm:w-fit inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    class="h-fit w-full sm:w-fit inline-flex items-center justify-center px-4 py-2 text-sm font-medium rounded-md shadow-sm"
+    :class="{
+      'border border-gray-300 text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500':
+        white,
+      'border border-transparent text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500':
+        !white,
+      'cursor-not-allowed opacity-70': disabled,
+    }"
   >
     <div class="flex gap-2 items-center">
       <FontAwesomeIcon :icon="leftIcon" v-if="leftIcon"></FontAwesomeIcon>
@@ -17,5 +24,7 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 defineProps<{
   leftIcon?: IconDefinition;
   rightIcon?: IconDefinition;
+  white?: boolean;
+  disabled?: boolean;
 }>();
 </script>
